2b9d6eb44789240745169e9407c477ed3f650e8f
vaibhavagarwa <vaibhavagrawal270@gmail.com>
2021-02-14 17:06:11 +0530

    wired output div

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var btnTranslate= document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-area");
+var outputDiv = document.querySelector("#output");
 
-console.log(txtInput); 
+//outputDiv.innerText="Here your output will appear";
+//or by directly writing in div tag in html
+
+
 
 function clickHandler(){
 
-    console.log("Clicked");
-    console.log("input",txtInput.value); 
-}
+    // console.log("Clicked");
+    // console.log("input",txtInput.value); 
+
+    outputDiv.innerText = "dkjksdb " + txtInput.value;
+};
 
 btnTranslate.addEventListener("click", clickHandler);
 //here "click" is tellling name of event and then calling function
+
+//console.log(outputDiv);
